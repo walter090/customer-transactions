0f0d00dbf4722238db540dd0f8670d1873f63daf
walter090 <walter.wu090@gmail.com>
2018-08-19 16:19:04 -0400

    Test transaction creation

--- operation/transaction/tests.py
+++ operation/transaction/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from .models import Transaction
 
-# Create your tests here.
+
+class TransactionTest(TestCase):
+    def test_creation(self):
+        entertainment_counts = Transaction.objects.filter(category='ENTERTAINMENT').count()
+        Transaction.objects.create(customer_id='000',
+                                   amount='300.00',
+                                   category='ENTERTAINMENT',
+                                   transfer_method='CARD')
+
+        self.assertEqual(Transaction.objects.filter(category='ENTERTAINMENT').count(),
+                         entertainment_counts + 1)
